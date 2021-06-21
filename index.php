a8f64d40610afab27a043fd26bbaab78bb97cf7d
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-06-21 16:53:32 +0500

    Updated

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (isset($_POST['add_button'])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Shopping Cart</title>
+	<title>Shopping Carts</title>
 	<!-- CSS only Bootstrap 5 -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<!-- CSS only Bootsrap 4.1.3 Needed to Cart -->
